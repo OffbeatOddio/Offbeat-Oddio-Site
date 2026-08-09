af33e0aa3831d12484570856b6d4c9accd4bebf8
Offbeat Oddio <offbeatoddio@protonmail.com>
2026-08-08 21:35:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,6 @@
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-} 
-
-
-
-
-
-
-
-
-
-
-
 const imagesList = [
-    {"src":"https://www.html-code-generator.com/images/slider/1.png","link":"","alt":"","name":"image 1"},
-    {"src":"https://www.html-code-generator.com/images/slider/2.png","link":"","alt":"","name":"image 2"},
-    {"src":"https://www.html-code-generator.com/images/slider/3.png","link":"","alt":"","name":"image 3"},
-    {"src":"https://www.html-code-generator.com/images/slider/4.png","link":"","alt":"","name":"image 4"},
-    {"src":"https://www.html-code-generator.com/images/slider/5.png","link":"","alt":"","name":"image 5"}
+    {"src":"exsub1.png","link":"","alt":"","name":"image 1"},
+    {"src":"exsub2.png","link":"","alt":"","name":"image 2"},
 ];
 
 function simpleSlider(selector, imagesList, options = {}) {
